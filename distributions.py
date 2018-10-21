fb698938eed10fc718db278a681d54f5f85ee727
leaprovenzano <leaprovenzano@gmail.com>
2018-10-21 18:07:25 +0100

    add veeery basic truncated norm dist

---
+++ distributions.py
@@ -0,0 +1,31 @@
+import math 
+
+import torch
+from torch.distributions import constraints
+from torch.distributions import Normal, Categorical
+
+
+class TruncatedNormal(Normal):
+
+    
+    arg_constraints = {'loc': constraints.real, 'scale': constraints.positive,
+                       'low': constraints.dependent, 'high': constraints.dependent}
+
+    def __init__(self, loc, scale,  lower=-float('inf'), upper=float('inf'), *args, **kwargs):
+        super(TruncatedNormal, self).__init__(loc, scale)
+        self.low, self.high = torch.ones_like(loc) * lower, torch.ones_like(loc) * upper
+
+        self._delta = (self.cdf(self.high) - self.cdf(self.cdf(self.low)))
+        self._delta[self.low>0] = -((self.cdf(-self.high) - self.cdf(self.cdf(-self.low))))[self.low>0]
+
+
+    def sample(self, sample_shape=torch.Size()):
+        shape = self._extended_shape(sample_shape)
+        with torch.no_grad():
+            u = torch.rand_like(self.loc)
+        return  self.icdf(self.cdf(self.low) +  u * self._delta)
+
+    def log_prob_trunc(self, value, eps=1e-5):
+        log_prob = self.log_prob(value) -  torch.log(self._delta)
+        log_prob[(value < self.low) | (value > self.high)] = math.log(0 + eps)
+        return log_prob
