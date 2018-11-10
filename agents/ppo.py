21896f4f7a8ba914c8f5be0e70052f86187c45c1
leaprovenzano <leaprovenzano@gmail.com>
2018-11-10 14:28:34 +0000

    fix from_saved method and logstr

--- agents/ppo.py
+++ agents/ppo.py
@@ -127,7 +127,7 @@ class PPOAgent(Agent):
 class MultiPPOAgent(object):
 
     @classmethod
-    def from_saved(cls, path, env_manager, memory, policy_model, value_model, policy_optimizer, value_optimizer):
+    def from_saved(cls, path, env_manager, policy_model, value_model, policy_optimizer, value_optimizer):
         cp = torch.load(path)
         agent = cls(env_manager, policy_model, value_model,
                     policy_optimizer, value_optimizer)
@@ -160,46 +160,53 @@ class MultiPPOAgent(object):
     def params(self):
         return {k: v for k, v in self.__dict__.items() if not hasattr(v, '__dict__')}
 
+    @property
+    def policy_std(self):
+        if type(self.policy_model.std) is float:
+            return self.policy_model.std
+        return np.exp(self.policy_model.std.detach().numpy()[0])
+
+
     def episode_logstr(self, train_stats):
-        eps_logstr = 'episode {}, reward avg {:.2f} |  policy std : {:.4f}'
+        eps_logstr='episode {}, reward avg {:.2f} | {} |  policy std : {:.4f}'
         return eps_logstr.format(self.episode, self.episode_rewards[-1],
                                  train_stats,
-                                 np.exp(self.policy_model.std.detach().numpy()[0]))
+                                 self.policy_std)
 
     def run_episode(self, render=False):
         self.envs.reset()
         while not self.envs.ready:
             with torch.no_grad():
-                actions, logprobs = self.policy_model.sample_action(self.envs.current_states)
+                actions, logprobs=self.policy_model.sample_action(self.envs.current_states)
             self.envs.step(actions, logprobs)
 
         self.episode_rewards.append(self.envs.mean_reward)
-        update_stats = self.train()
+        update_stats=self.train()
         print(self.episode_logstr(update_stats))
         self.episode += 1
 
     def train(self):
-        _, nsamples = self.update()
+        _, nsamples=self.update()
         return 'nsamples : {}'.format(nsamples)
 
     def estimate_advantage(self, returns, states):
-        advantages = Variable(returns).view(-1, 1) - Variable(self.value_model.predict(states).data).view(-1, 1)
+        advantages=Variable(returns).view(-1, 1) - Variable(self.value_model.predict(states).data).view(-1, 1)
         if self.normalize_advantage:
-            advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-5)
+            advantages=(advantages - advantages.mean()) / (advantages.std() + 1e-5)
         return advantages
 
     def policy_loss(self, states, actions, old_log_probs, advantages):
-        log_probs = self.policy_model.evaluate_action(states, actions)
-        ratio = torch.exp(log_probs - Variable(old_log_probs.data))
-        surr1 = ratio * advantages
-        surr2 = torch.clamp(ratio, 1.0 - self.clip_epsilon, 1.0 + self.clip_epsilon) * advantages
+        log_probs=self.policy_model.evaluate_action(states, actions)
+        ratio=torch.exp(log_probs - Variable(old_log_probs.data))
+        surr1=ratio * advantages
+        surr2=torch.clamp(ratio, 1.0 - self.clip_epsilon, 1.0 + self.clip_epsilon) * advantages
         return -torch.min(surr1, surr2).mean()
 
     def value_loss(self, y_true, y_pred):
         return (0.5 * (y_true.view(-1, 1) - y_pred.view(-1, 1)).pow(2)).mean()
 
     def policy_step(self, states, actions, old_log_probs, advantages):
-        loss = self.policy_loss(states, actions, old_log_probs, advantages)
+        loss=self.policy_loss(states, actions, old_log_probs, advantages)
         self.policy_optimizer.zero_grad()
         loss.backward()
         nn.utils.clip_grad_norm_(self.policy_model.parameters(), self.policy_grad_norm)
@@ -207,8 +214,8 @@ class MultiPPOAgent(object):
         return loss.item()
 
     def value_step(self, states, returns):
-        value_pred = self.value_model.predict(states)
-        value_loss = self.value_loss(returns, value_pred)
+        value_pred=self.value_model.predict(states)
+        value_loss=self.value_loss(returns, value_pred)
         self.value_optimizer.zero_grad()
         value_loss.backward()
         nn.utils.clip_grad_norm_(self.value_model.parameters(), self.value_grad_norm)
@@ -216,53 +223,53 @@ class MultiPPOAgent(object):
         return value_loss.item()
 
     def update_policy(self, states, actions, returns, old_log_prob):
-        losses = []
-        advantages = self.estimate_advantage(returns, states)
+        losses=[]
+        advantages=self.estimate_advantage(returns, states)
         for i in range(self.policy_epochs):
             losses.append(self.policy_step(states, actions, old_log_prob, advantages))
         self.history.update_policy_losses(losses)
 
     def update_value_model(self, states, returns):
-        losses = []
+        losses=[]
         for i in range(self.value_epochs):
             losses.append(self.value_step(states, returns))
         self.history.update_value_losses(losses)
 
     def update(self):
-        states, actions, returns, logprobs = self.envs.get_train_data()
+        states, actions, returns, logprobs=self.envs.get_train_data()
 
-        states = Variable(states)
-        actions = Variable(actions)
-        returns = Variable(returns)
-        logprobs = Variable(logprobs.data)
+        states=Variable(states)
+        actions=Variable(actions)
+        returns=Variable(returns)
+        logprobs=Variable(logprobs.data)
 
         self.update_policy(states, actions, returns, logprobs)
         self.update_value_model(states, returns)
         return str(self.history), len(states)
 
     def save(self, path):
-        out = {'attrs': self.params}
-        state_dicts = {}
+        out={'attrs': self.params}
+        state_dicts={}
 
-        state_dicts['policy_model'] = self.policy_model.state_dict()
-        state_dicts['value_model'] = self.value_model.state_dict()
-        state_dicts['policy_optimizer'] = self.policy_optimizer.state_dict()
-        state_dicts['value_optimizer'] = self.value_optimizer.state_dict()
+        state_dicts['policy_model']=self.policy_model.state_dict()
+        state_dicts['value_model']=self.value_model.state_dict()
+        state_dicts['policy_optimizer']=self.policy_optimizer.state_dict()
+        state_dicts['value_optimizer']=self.value_optimizer.state_dict()
 
-        out['state_dicts'] = state_dicts
+        out['state_dicts']=state_dicts
         torch.save(out, path)
 
     def run_test_episode(self, env, render=True):
-        reward = 0
-        state = env.reset()
-        done = False
+        reward=0
+        state=env.reset()
+        done=False
         while not done:
 
             with torch.no_grad():
-                state = torch.FloatTensor(state).view(1, -1)
-                action = self.policy_model(state)
+                state=torch.FloatTensor(state).view(1, -1)
+                action=self.policy_model(state)
 
-            state, r, done, _ = env.step(np.asarray(action)[0])
+            state, r, done, _=env.step(np.asarray(action)[0])
             reward += r
             env.render()
 
