b6abdebabc9dd1f0cee1c2bbcd1e9e210c003793
leaprovenzano <leaprovenzano@gmail.com>
2018-11-10 13:21:30 +0000

    added plot func

--- utils.py
+++ utils.py
@@ -2,6 +2,21 @@ import torch
 import numpy as np
 
 
+class MissingDependency(Exception):
+    pass
+
+
+def plot_rewards(agent):
+    try:
+        import pandas as pd
+    except ImportError as e:
+        raise MissingDependency("{}. The plot_rewards method requires pandas (because dev was lazy) if you want you can 'pip install pandas'".format(e))
+    df = pd.DataFrame()
+    df['reward'] = pd.Series(agent.episode_rewards)
+    df['mean_100_episide_reward'] = df.reward.rolling(window=100, min_periods=0).mean()
+    return df.plot()
+
+
 class RewardProcessor(object):
 
     def __init__(self, gamma=.99, positive_factor=2., clamp=(-1, 1)):
