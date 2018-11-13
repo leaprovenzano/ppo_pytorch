af50b9a3f81466676978db48e2cb69b8763f96a2
leaprovenzano <leaprovenzano@gmail.com>
2018-11-13 08:28:16 +0000

    import layers into models so easier to access

---
+++ ppo_pytorch/models/__init__.py
@@ -0,0 +1,3 @@
+from ppo_pytorch.models.shared import PolicyValueModel
+from ppo_pytorch.models.policy_layers import CategoricalPolicy, GaussianPolicy, FixedGaussianPolicy, TruncatedGaussianPolicy, FixedTruncatedGaussianPolicy
+from ppo_pytorch.models.value_layers import ValueLayer
